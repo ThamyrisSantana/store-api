4bafcae09d9086780e9bb9eb9545a336bf376c68
ThamyrisSantana <thamyrissatana3@gmail.com>
2024-01-05 21:09:56 -0300

    feat: create app config

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+
+import express from "express";
+import mongoose from "mongoose";
+
+import productRoutes from "./routes/product";
+import authRoutes from "./routes/user";
+
+mongoose.connect(
+	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@star-wars-api.esry0eo.mongodb.net/?retryWrites=true&w=majority`
+);
+
+const port = 3030;
+const app = express();
+
+app.use(express.json());
+app.use("/product", productRoutes);
+app.use("/auth", authRoutes);
+
+app.listen(port, () => {
+	console.log(`Listening on: http://localhost:${port}`);
+});
